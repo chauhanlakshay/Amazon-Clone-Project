1efed6815e6610db680d223e5c692ac5e40139df
Lakshay <imlakshay@gmail.com>
2021-12-20 15:37:54 +0530

    Complete Project

---
+++ functions/index.js
@@ -0,0 +1,37 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51IE9CMK7w0fZQidOLGP2ba5d5zkEnV1StTe99lNi3qWKspGHU1P7r5mFoomFKRyOxNTBWoWuBBgK0Rq7lZpKf5Su00DOARaAtm')
+
+//API
+// - App config
+const app = express();
+
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "inr",
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
+
+// Example endpoint
+// http://localhost:5001/challenge-4b2b2/us-central1/api
